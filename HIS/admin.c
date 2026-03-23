9b9475f1d6db9cbdd2d9af0366a86547eda8e8b5
jerry D <djw20240623@outlook.com>
2026-03-23 12:45:57 +0800

    today_change

---
+++ HIS/admin.c
@@ -0,0 +1,11 @@
+#include "admin.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void adminMenu() {
+    system("cls");
+    printf("=== 后台管理与分析中心 ===\n");
+    printf(">> 提示：此模块由管理端负责同学接入。\n");
+    printf(">> 请在此处调用药房管理、智能预估调控、排班管理、财务报表导出等子菜单...\n");
+    system("pause");
+}
