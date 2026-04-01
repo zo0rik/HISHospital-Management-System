d3e23c7696e6d1b2fa3d371a04b668f3f106d7d0
Lapras-Pushkin <15216033126@139.com>
2026-04-01 20:08:37 +0800

    陈展鹏提交
    * Refactor schedule management functions in schedule.c
    
    * Update doctor.c
    
    * 34t3
    
    * ho~ho~~
    
    ---------
    
    Co-authored-by: Lapras_pushkin <联想@LAPTOP-H1URKEJP>

--- HIS_text/time_t.c
+++ HIS_text/time_t.c
@@ -18,3 +18,64 @@ void getCurrentTime(char* buffer, int size) {
     // 3. 将 tm 结构体内的离散时间格式化为符合 YYYY-MM-DD HH:MM:SS 的标准串
     strftime(buffer, size, "%Y-%m-%d %H:%M:%S", tm_info);
 }
+//用于时间格式输入的校验，确保用户输入的时间符合 YYYY-MM-DD 的格式，并且日期合法
+void judgetime(char* end) {
+    while (1) {
+        scanf("%11s", end);
+        for (int i = 0;i < 10;i++) {
+            if (i == 4 || i == 7) {
+                if (end[i] != '-') {
+                    printf("格式错误，请重新输入: ");
+                    continue;
+                }
+            }
+            else if (end[i] < '0' || end[i]>'9') {
+                printf("格式错误，请重新输入: ");
+                continue;
+            }
+        }
+        if (end[10] != '\0') {
+            printf("格式错误，请重新输入: ");
+            continue;
+        }
+        int year = (end[0] - '0') * 1000 + (end[1] - '0') * 100 + (end[2] - '0') * 10 + (end[3] - '0');
+        int month = (end[5] - '0') * 10 + (end[6] - '0');
+        int day = (end[8] - '0') * 10 + (end[9] - '0');
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            if (month < 1 || month>12) {
+                printf("格式错误，请重新输入: ");
+                continue;
+            }
+            if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && (day < 1 || day>31)) {
+                printf("格式错误，请重新输入: ");
+                continue;
+            }
+            if ((month == 4 || month == 6 || month == 9 || month == 11) && (day < 1 || day>30)) {
+                printf("格式错误，请重新输入: ");
+                continue;
+            }
+            if (month == 2 && (day < 1 || day>29)) {
+                printf("格式错误，请重新输入: ");
+                continue;
+            }
+        }
+        else {
+            if (month < 1 || month>12) {
+                printf("格式错误，请重新输入: ");
+                continue;
+            }
+            if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && (day < 1 || day>31)) {
+                printf("格式错误，请重新输入: ");
+                continue;
+            }
+            if ((month == 4 || month == 6 || month == 9 || month == 11) && (day < 1 || day>30)) {
+                printf("格式错误，请重新输入: ");
+                continue;
+            }
+            if (month == 2 && (day < 1 || day>28)) {
+                printf("格式错误，请重新输入: ");
+                continue;
+            }
+        }
+    }
+}
